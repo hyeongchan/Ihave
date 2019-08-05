e6754b01f624462005a76da146236f6d0746f6cf
Ham <hyoungchan@unist.ac.kr>
2019-08-06 02:19:01 +0900

    first commit

---
+++ cookapp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
+
+class Ingredient(models.Model):
+    code = models.IntegerField(null=True)
+    name = models.CharField(max_length=10, null=True)
+    category = models.CharField(max_length = 10, null=True)
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=255, null=True)
+    url = models.URLField(null=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    ingredients = models.ManyToManyField(Ingredient)
+    image_thumbnail = ImageSpecField(source = 'image', processors=[ResizeToFill(120,60)])
+
+    def __str__(self):
+        return self.name
