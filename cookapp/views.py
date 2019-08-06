786c45d089c98ffc81449aec46a41fbde0d45253
Ubuntu <ubuntu@ip-172-31-39-80.ap-northeast-2.compute.internal>
2019-08-06 11:08:42 +0000

    server published

--- cookapp/views.py
+++ cookapp/views.py
@@ -3,42 +3,22 @@ from .models import Ingredient, Recipe
 
 def home(request):
     if request.method == 'GET':
-        print("one")
         ingredients = Ingredient.objects
         categories = Ingredient.objects.values_list('category', flat=True)
         categories = list(set(categories))
-        code = [1, 2, 3, 4, 5]
-        return render(request, 'home.html', {'ingredients' : ingredients, 'code' : code, 'categories' : categories})
+        return render(request, 'home.html', {'ingredients' : ingredients, 'categories' : categories})
     else :
-        print("in")
         ingr = Ingredient.objects.values_list('name', flat=True)
-        print("1")
         full_list = list(ingr)
-        print("2")
         all = Recipe.objects
-        print("all : ",all.all())
-        print("3")
         choose = ["쌀밥", "삼겹살", "조밥", "요거트", "된장", "우유", "소면", "잡곡", "등심", "고추장"]
-        print("4")
         sub = set(full_list) - set(choose)
-        print("5")
         recipe = all
-        print("recipe : ",recipe)
-        print("sub : ",sub)
-        print("6")
         for r in sub:
-            print("r : ",r)
             recipe = recipe.exclude(ingredients__name = r)
-        print("end for")
-        print(recipe)
-        print("7")
         add = []
         for r in all.all():
-            print("loop")
             sub = set(r.ingredients.values_list('name', flat=True)) - set(choose)
-            print("sub : ",sub)
             if len(sub) == 1:
                 add.append(r)
-            print("add : ",add)
-        print("8")
         return render(request, 'list.html', {'all':all, 'recipes':recipe, 'add':add})
--- cookproj/settings.py
+++ cookproj/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '3+d@_zbh6*rz&54ry!x=5a9fk9=w6fymv5njaoc7@b__ju1@-+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '.ap-northeast-2.compute.amazonaws.com',
+]
 
 
 # Application definition
@@ -76,18 +79,18 @@ WSGI_APPLICATION = 'cookproj.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # },
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django',
-        'USER': 'root',
-        'PASSWORD': 'gudcks98!',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    },
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     },
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'django',
+#        'USER': 'root',
+#        'PASSWORD': 'gudcks98!',
+#        'HOST': '127.0.0.1',
+#        'PORT': '3306',
+#    },
 }
 
 
