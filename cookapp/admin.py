3ebf7034b0aebcf73dc917929a1751330bdd715c
Ham <hyoungchan@unist.ac.kr>
2019-08-07 22:00:03 +0900

    add data in local

--- cookapp/admin.py
+++ cookapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Ingredient, Recipe
+from .models import Ingredient, Recipe, Category
 
 admin.site.register(Ingredient)
 admin.site.register(Recipe)
+admin.site.register(Category)
